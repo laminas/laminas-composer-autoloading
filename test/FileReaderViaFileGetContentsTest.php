04b6fb950d0434ce6d332f4026f8ab2e7f024a17
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-03-24 10:31:51 -0500

    qa: adds tests for FileReaderViaFileGetContents
    Tests all paths through implementation, using vfsStream for file operations.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

---
+++ test/FileReaderViaFileGetContentsTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-composer-autoloading for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-composer-autoloading/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-composer-autoloading/blob/master/LICENSE.md New BSD License
+ */
+
+namespace LaminasTest\ComposerAutoloading;
+
+use Laminas\ComposerAutoloading\Exception\ComposerJsonFileException;
+use Laminas\ComposerAutoloading\FileReaderViaFileGetContents;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+use PHPUnit\Framework\TestCase;
+
+class FileReaderViaFileGetContentsTest extends TestCase
+{
+    /** @var vfsStreamDirectory */
+    private $dir;
+
+    public function setUp(): void
+    {
+        $this->dir = vfsStream::setup('root', null, [
+            'config' => [
+                'config.php' => '<?php return [];',
+            ],
+            'module' => [],
+        ]);
+    }
+
+    public function testRaisesExceptionIfFileDoesNotExist(): void
+    {
+        $filename = vfsStream::url('root/composer.json');
+        $reader   = new FileReaderViaFileGetContents();
+
+        $this->expectException(ComposerJsonFileException::class);
+        $this->expectExceptionMessage('does not exist');
+        $reader($filename);
+    }
+
+    public function testRaisesExceptionIfFileIsNotReadable(): void
+    {
+        vfsStream::newFile('composer.json', 0222)->at($this->dir);
+        $filename = vfsStream::url('root/composer.json');
+        $reader   = new FileReaderViaFileGetContents();
+
+        $this->expectException(ComposerJsonFileException::class);
+        $this->expectExceptionMessage('unreadable');
+        $reader($filename);
+    }
+
+    public function testReturnsFileContentsIfFileExistsAndIsReadable(): void
+    {
+        $fileContent = '{"name": "laminas/laminas-composer-autoloading"}';
+        vfsStream::newFile('composer.json')->at($this->dir)->withContent($fileContent);
+
+        $filename = vfsStream::url('root/composer.json');
+        $reader   = new FileReaderViaFileGetContents();
+
+        $this->assertSame($fileContent, $reader($filename));
+    }
+}
